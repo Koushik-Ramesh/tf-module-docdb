65fdde45e85543a4ab48b7c308c95ef36ad6d481
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-08 15:32:47 +0530

    Autopush

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 # Reads the information from remote state file
 data "terraform_remote_state" "vpc" {
     backend = "s3"
-    config {
+    config = {
         bucket = "koushik-bucket"
         key = "vpc/${var.ENV}/terraform.tfstate"
         region = "us-east-1"
