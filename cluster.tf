f587e100443bef08a47c42bcfd84c8a221225ab3
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-08 15:26:16 +0530

    Autopush

--- cluster.tf
+++ cluster.tf
@@ -3,17 +3,17 @@ resource "aws_docdb_cluster" "docdb" {
   engine                  = "docdb"
   master_username         = "admin1"
   master_password         = "roboshop1"
-  preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true                        # Value will be false in production. In Lab, we will be using true
-  db_subnet_group_name = 
+  db_subnet_group_name    = aws_docdb_subnet_group.DOCDB.name
 }
 
 # Creates DocDB Subnet group
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-${var.ENV}-docdb-subnet-group"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID
 
   tags = {
-    Name = "My docdb subnet group"
+    Name = "roboshop-${var.ENV}-docdb-subnet-group"
   }
 }
+
