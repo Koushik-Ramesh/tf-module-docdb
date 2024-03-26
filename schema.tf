ecfeaa0102502d37c012ce1614ad81062e2d13a8
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-26 15:03:09 +0530

    Autopush

--- schema.tf
+++ schema.tf
@@ -6,11 +6,12 @@ resource "null_resource" "schema" {
             wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
             curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
             cd /tmp
-            unzip mongodb.zip
+            unzip -o /tmp/mongodb.zip
             cd mongodb-main
             ls -ltr
-            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1  < catalogue.js
-            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < users.js          
+            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1  < catalogue.js       
         EOF
     }
 }
+
+#            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < users.js   
