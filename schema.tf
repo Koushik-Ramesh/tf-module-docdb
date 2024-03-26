783505378c9ee7c78571a574bb832db2dfbdab84
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-26 14:26:23 +0530

    Autopush

--- schema.tf
+++ schema.tf
@@ -6,9 +6,9 @@ resource "null_resource" "schema" {
         command = <<EOF
             cd /tmp
             wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-            curl -s -L -o /tmp/mongo.zip "https://github.com/stans-robot-project/mongo/archive/main.zip"
-            unzip -o mongo.zip
-            cd /tmp/mongo-main
+            curl -s -L -o /tmp/mongo.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+            unzip -o mongodb.zip
+            cd mongodb-main
             mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1  < catalogue.js
             mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1  < users.js           
 
